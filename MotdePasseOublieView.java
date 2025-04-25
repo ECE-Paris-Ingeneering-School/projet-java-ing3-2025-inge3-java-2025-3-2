4ba23e2c1413a9735ff225be84922d3aba5baffc
Loan <131166084+loanper@users.noreply.github.com>
2025-04-25 15:04:11 +0200

    Create MotdePasseOublieView.java
    page si l'on clique sur mot de passe oublié

---
+++ MotdePasseOublieView.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MotDePasseOublieView extends JPanel {
+
+    JTextField mailField;
+    JButton envoyerLien;
+
+    public MotDePasseOublieView(ApplicationFrame parent) {
+        setLayout(new BorderLayout(10, 10));
+
+        JLabel instruction = new JLabel("Entrez votre adresse mail pour recevoir le lien de récupération :");
+        mailField = new JTextField(20);
+        envoyerLien = new JButton("Envoyer lien");
+
+        envoyerLien.addActionListener(e -> JOptionPane.showMessageDialog(this, "Lien envoyé à " + mailField.getText()));
+
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(mailField);
+        panel.add(envoyerLien);
+
+        add(instruction, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+    }
+}
