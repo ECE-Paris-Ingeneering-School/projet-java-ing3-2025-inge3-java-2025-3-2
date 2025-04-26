2d678185c5d4565eeef0b930ca2ff5e0eafada03
Thomas-C06 <131240321+Thomas-C06@users.noreply.github.com>
2025-04-26 12:05:50 +0200

    Update UtilisateurDAO.java

--- UtilisateurDAO.java
+++ UtilisateurDAO.java
@@ -1,25 +1,29 @@
 package DAO;
 
-import modele.Utilisateur;
+import Modele.Utilisateur;
 import java.sql.*;
 
 public class UtilisateurDAO {
-    public Utilisateur getByEmail(String email) {
-        String query = "SELECT * FROM Utilisateur WHERE Email = ?";
+
+    public Utilisateur getByMail(String mail) {
+        String query = "SELECT * FROM utilisateur WHERE mail = ?";
         try (Connection conn = ConnexionDB.getConnection();
              PreparedStatement ps = conn.prepareStatement(query)) {
 
-            ps.setString(1, email);
+            ps.setString(1, mail);
             ResultSet rs = ps.executeQuery();
+
             if (rs.next()) {
-                return new Utilisateur(rs.getInt("ID_Utilisateur"),
-                        rs.getString("Nom"),
-                        rs.getString("Email"),
-                        rs.getString("Mot_de_Passe"),
-                        rs.getString("Type_Utilisateur")) {
+                return new Utilisateur(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getString("mail"),
+                        rs.getString("motDePasse"),
+                        rs.getString("type")
+                ) {
                     @Override
                     public void seConnecter() {
-                        System.out.println("Utilisateur connecté : " + getNom());
+                        System.out.println("Connexion utilisateur : " + getNom());
                     }
                 };
             }
